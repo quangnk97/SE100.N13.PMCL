54d8565864faea3ec6835d67a593b887830dc472
Quang-UIT20521812 <quangnkdnvn2002@gmail.com>
2022-11-13 13:09:00 +0700

    Add project files.

---
+++ QuanLyDeTai/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyDeTai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

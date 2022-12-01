b8d8826cb3bdbdf8bb3e7fd15e6de93b12099a9e
NguyenPhuc <20521767@gm.uit.edu.vn>
2022-12-01 11:04:44 +0700

    Just test

---
+++ QuanLyDeTai/Areas/Admin/Controllers/AdminLecturersController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuanLyDeTai.Models;
+
+namespace QuanLyDeTai.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminLecturersController : Controller
+    {
+        private readonly QLDT_DbContext _context;
+
+        public AdminLecturersController(QLDT_DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminLecturers
+        public async Task<IActionResult> Index()
+        {
+            var qLDT_DbContext = _context.Lecturers.Include(l => l.Faculty);
+            return View(await qLDT_DbContext.ToListAsync());
+        }
+
+        // GET: Admin/AdminLecturers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Lecturers == null)
+            {
+                return NotFound();
+            }
+
+            var lecturer = await _context.Lecturers
+                .Include(l => l.Faculty)
+                .FirstOrDefaultAsync(m => m.LecturerId == id);
+            if (lecturer == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecturer);
+        }
+
+        // GET: Admin/AdminLecturers/Create
+        public IActionResult Create()
+        {
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId");
+            return View();
+        }
+
+        // POST: Admin/AdminLecturers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LecturerId,LecturerCode,LecturerName,Dob,Major,Email,PhoneNumber,Contract,LevelCurrent,FacultyId,Avatar")] Lecturer lecturer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lecturer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId", lecturer.FacultyId);
+            return View(lecturer);
+        }
+
+        // GET: Admin/AdminLecturers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Lecturers == null)
+            {
+                return NotFound();
+            }
+
+            var lecturer = await _context.Lecturers.FindAsync(id);
+            if (lecturer == null)
+            {
+                return NotFound();
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId", lecturer.FacultyId);
+            return View(lecturer);
+        }
+
+        // POST: Admin/AdminLecturers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LecturerId,LecturerCode,LecturerName,Dob,Major,Email,PhoneNumber,Contract,LevelCurrent,FacultyId,Avatar")] Lecturer lecturer)
+        {
+            if (id != lecturer.LecturerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lecturer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LecturerExists(lecturer.LecturerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId", lecturer.FacultyId);
+            return View(lecturer);
+        }
+
+        // GET: Admin/AdminLecturers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Lecturers == null)
+            {
+                return NotFound();
+            }
+
+            var lecturer = await _context.Lecturers
+                .Include(l => l.Faculty)
+                .FirstOrDefaultAsync(m => m.LecturerId == id);
+            if (lecturer == null)
+            {
+                return NotFound();
+            }
+
+            return View(lecturer);
+        }
+
+        // POST: Admin/AdminLecturers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Lecturers == null)
+            {
+                return Problem("Entity set 'QLDT_DbContext.Lecturers'  is null.");
+            }
+            var lecturer = await _context.Lecturers.FindAsync(id);
+            if (lecturer != null)
+            {
+                _context.Lecturers.Remove(lecturer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LecturerExists(int id)
+        {
+          return _context.Lecturers.Any(e => e.LecturerId == id);
+        }
+    }
+}

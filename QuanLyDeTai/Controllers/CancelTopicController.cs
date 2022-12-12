51598675dc128e15410e527d03b19bd22d7bcc5c
NguyenPhuc <20521767@gm.uit.edu.vn>
2022-12-12 12:14:50 +0700

    UI cancel topic & Extend time limit

--- QuanLyDeTai/Controllers/CancelTopicController.cs
+++ QuanLyDeTai/Controllers/CancelTopicController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyDeTai.Models;
 
 namespace QuanLyDeTai.Controllers
 {
     public class CancelTopicController : Controller
     {
+        private readonly QLDT_DbContext _context;
+
+        public CancelTopicController(QLDT_DbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Cancel()
+        {
+            return RedirectToAction(nameof(Index));
         }
     }
 }

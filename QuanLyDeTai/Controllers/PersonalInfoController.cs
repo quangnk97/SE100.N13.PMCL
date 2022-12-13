5330ddb077bfd8b347f3c7ecddcafb4b00128b86
NguyenPhuc <20521767@gm.uit.edu.vn>
2022-12-13 13:08:07 +0700

    UI Personal info

--- QuanLyDeTai/Controllers/PersonalInfoController.cs
+++ QuanLyDeTai/Controllers/PersonalInfoController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyDeTai.Models;
 
 namespace QuanLyDeTai.Controllers
 {
     public class PersonalInfoController : Controller
     {
+        private readonly QLDT_DbContext _context;
+
+        public PersonalInfoController(QLDT_DbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();

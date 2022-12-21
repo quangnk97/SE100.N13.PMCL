d0b5b81f01cc7272094d05c7f75acd0a725e802c
NguyenPhuc <20521767@gm.uit.edu.vn>
2022-12-21 21:06:29 +0700

    get current logged in user

--- QuanLyDeTai/Controllers/TopicRegisterController.cs
+++ QuanLyDeTai/Controllers/TopicRegisterController.cs
@@ -5,7 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using QuanLyDeTai.Models;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace QuanLyDeTai.Controllers
 {
@@ -27,8 +33,45 @@ namespace QuanLyDeTai.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register()
+        public async Task<IActionResult> Register([Bind]Topic topic, List<Lecturer> selectedLecturers)
         {
+            TempData["abc"] = User.Identity.GetUserId();
+            if (String.IsNullOrEmpty(topic.TopicName))
+            {
+                if (String.IsNullOrEmpty(topic.ResearchField))
+                {
+                    TempData["TopicNameEmpty"] = "Required.";
+                    TempData["ResearchFieldEmpty"] = "Required.";
+                }
+                else
+                {
+                    TempData["TopicNameEmpty"] = "Required.";
+                }
+            }
+            else if (String.IsNullOrEmpty(topic.ResearchField))
+            {
+                if (String.IsNullOrEmpty(topic.TopicName))
+                {
+                    TempData["TopicNameEmpty"] = "Required.";
+                    TempData["ResearchFieldEmpty"] = "Required.";
+                }
+                else
+                {
+                    TempData["ResearchFieldEmpty"] = "Required.";
+                }
+            }
+            else
+            {
+                topic.RegisteredYear = DateTime.Now.Year;
+                topic.Duration = 365;
+                topic.Approved = false;
+                topic.IsExtended = false;
+                topic.IsCancelled = false;
+                //_context.Add(topic);
+                //await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
             return RedirectToAction(nameof(Index));
         }
     }

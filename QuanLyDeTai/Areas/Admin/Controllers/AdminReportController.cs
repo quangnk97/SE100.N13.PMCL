799157de3fa7d79f85f6250ec929f64e6fa1b080
Quang-UIT20521812 <quangnkdnvn2002@gmail.com>
2023-02-08 04:27:39 +0700

    Fix2

--- QuanLyDeTai/Areas/Admin/Controllers/AdminReportController.cs
+++ QuanLyDeTai/Areas/Admin/Controllers/AdminReportController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuanLyDeTai.Models;
 
 namespace QuanLyDeTai.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminReportController : Controller
     {
-        public IActionResult Index()
+        private readonly QLDT_DbContext _context;
+
+        public AdminReportController(QLDT_DbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string searchTopic)
+        {
+            var topic = await _context.Topics.FirstOrDefaultAsync(x => x.TopicName == searchTopic);
+            ViewData["AssessmentId"] = new SelectList(_context.Assessments, "AssessmentId", "AssessmentId");
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "StatusId", "StatusName");
+            ViewData["SearchTopic"] = searchTopic;
+            return View(topic);
         }
     }
 }
